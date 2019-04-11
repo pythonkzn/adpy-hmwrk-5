a7869859a4f6bbb65e247fc9ef7d8bfad507719a
raravilov <raravilov@gmail.com>
2019-04-11 10:54:39 +0300

    доработки

--- adpy-pep.py
+++ adpy-pep.py
@@ -7,49 +7,55 @@ from email.mime.multipart import MIMEMultipart
 
 GMAIL_SMTP = 'smtp.gmail.com'
 GMAIL_IMAP = 'imap.gmail.com'
-LOGIN = 'raravilov@gmail.com'
-PASSWORD = 'Ruslan123!'
+LOGIN = '...'
+PASSWORD = '...!'
 SUBJECT = 'Subject'
-RECIPIENTS = ['raravilov@bars.group', 'raravilov@gmail.com']
+RECIPIENTS = ['...', '...']
 MESSAGE = 'Message'
 HEADER = None
 
 
 class MailTool:
-    def __init__(self, smtp):
-        self.smtp = smtplib.SMTP(smtp, 587)
+    def __init__(self, smtp_in, imap_in):
+        self.smtp = smtplib.SMTP(smtp_in, 587)
+        self.imap = imaplib.IMAP4_SSL(imap_in)
 
-    def send_email(self):
+    def send_email(self, msg_in):
         self.smtp.ehlo()
         self.smtp.starttls()
         self.smtp.ehlo()
         self.smtp.login(LOGIN, PASSWORD)
-        self.smtp.send_message(msg)
+        self.smtp.send_message(msg_in)
         self.smtp.quit()
 
-        
-msg = MIMEMultipart()
-msg['From'] = LOGIN
-msg['To'] = ', '.join(RECIPIENTS)
-msg['Subject'] = SUBJECT
-msg.attach(MIMEText(MESSAGE))
-ms = MailTool(GMAIL_SMTP)
-ms.send_email()
+    def receive_email(self, criterion_in):
+        self.imap.login(LOGIN, PASSWORD)
+        self.imap.list()
+        self.imap.select('inbox')
+        result_out, data_out = self.imap.uid('search', None, criterion_in)
+        assert data_out[0], 'There are no letters with current header'
+        latest_email_uid = data_out[0].split()[-1]
+        result_out, data_out = self.imap.uid('fetch', latest_email_uid, '(RFC822)')
+        raw_email_out = data_out[0][1]
+        email_message_out = email.message_from_string(str(raw_email_out))
+        self.imap.logout()
+        return email_message_out
 
 
+def main():
+    # send
+    msg = MIMEMultipart()
+    msg['From'] = LOGIN
+    msg['To'] = ', '.join(RECIPIENTS)
+    msg['Subject'] = SUBJECT
+    msg.attach(MIMEText(MESSAGE))
+    ms = MailTool(GMAIL_SMTP, GMAIL_IMAP)
+    ms.send_email(msg)
+
+    # receive
+    criterion = '(HEADER Subject {})'.format(HEADER) if HEADER else 'ALL'
+    email_message = ms.receive_email(criterion)
 
 
-#recieve
-#mail = imaplib.IMAP4_SSL(GMAIL_IMAP)
-#mail.login(LOGIN, PASSWORD)
-#mail.list()
-#mail.select("inbox")
-#criterion = '(HEADER Subject "%s")' % HEADER if HEADER else 'ALL'
-#result, data = mail.uid('search', None, criterion)
-#assert data[0], 'There are no letters with current header'
-#latest_email_uid = data[0].split()[-1]
-#result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-#raw_email = data[0][1]
-#email_message = email.message_from_string(raw_email)
-#mail.logout()
-#end recieve
+if __name__ == '__main__':
+    main()
